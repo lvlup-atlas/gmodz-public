a2a6c31f860e87e39a8691698dd1c7f107353bd5
youremeh <youremeh@proton.me>
2025-12-27 02:45:50 -0500

    Add safe zones, banks, and shops for gm_construct

--- dayz/gamemode/scripts/shared/shar_safezone.lua
+++ dayz/gamemode/scripts/shared/shar_safezone.lua
@@ -1,8 +1,11 @@
 SafeZones = {}
 SafeZones["rp_stalker_new"] = {{Pos = Vector(512, -11776, -256), Radius = 1400}}
+SafeZones["gm_construct"] = {{Pos = Vector(0, 0, 0), Radius = 1400}}
 
 Banks = {}
 Banks["rp_stalker_new"] = {Vector(512, -11777, -256)}
+Banks["gm_construct"] = {Vector(0, 0, 0)}
 
 Shops = {}
 Shops["rp_stalker_new"] = {Vector(217.51, -11582.32, -639.97)}
+Shops["gm_construct"] = {Vector(0, 0, 0)}
